1952ec2f74ff72f13451a75c0a39afd72f67fa88
Otfataok <nikolay.ermishin@gmail.com>
2025-12-07 19:10:49 +0100

    Add i18n functions to module exports

--- libs/ktem/ktem/utils/__init__.py
+++ libs/ktem/ktem/utils/__init__.py
@@ -1,4 +1,5 @@
 from .conversation import get_file_names_regex, get_urls
 from .lang import SUPPORTED_LANGUAGE_MAP
+from .i18n import get_language, set_language, get_text, t, LANGUAGES
 
 __all__ = ["SUPPORTED_LANGUAGE_MAP", "get_file_names_regex", "get_urls"]
